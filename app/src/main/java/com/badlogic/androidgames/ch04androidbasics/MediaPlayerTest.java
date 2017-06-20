7d0e3db5425330718e62d256aa107e2f21200e55
Josh <pippijr0@sewanee.edu>
2017-06-19 22:02:31 -0500

    MediaPlayerTest and SoundPoolTest

---
+++ app/src/main/java/com/badlogic/androidgames/ch04androidbasics/MediaPlayerTest.java
@@ -0,0 +1,64 @@
+package com.badlogic.androidgames.ch04androidbasics;
+
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+/**
+ * Created by Josh on 6/19/2017.
+ */
+
+public class MediaPlayerTest extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        setContentView(textView);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        mediaPlayer = new MediaPlayer();
+
+        try {
+            AssetManager assetManager = getAssets();
+            AssetFileDescriptor descriptor = assetManager.openFd("music.ogg");
+            mediaPlayer.setDataSource(descriptor.getFileDescriptor(),descriptor.getStartOffset(),descriptor.getLength());
+            mediaPlayer.prepare();
+            mediaPlayer.setLooping(true);
+
+        } catch (IOException e) {
+            textView.setText("Couldn't load music file, " + e.getMessage());
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+    }
+
+    protected void onPause() {
+        super.onPause();
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+            if (isFinishing()) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+            }
+        }
+    }
+
+
+}
