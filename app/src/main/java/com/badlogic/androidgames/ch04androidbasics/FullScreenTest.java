ee7e1152aa16d471f9bd67382ffd0cbca74ec809
Josh <pippijr0@sewanee.edu>
2017-06-20 21:58:28 -0500

    FullScreenTest and RenderViewTest

---
+++ app/src/main/java/com/badlogic/androidgames/ch04androidbasics/FullScreenTest.java
@@ -0,0 +1,20 @@
+package com.badlogic.androidgames.ch04androidbasics;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Josh on 6/20/2017.
+ */
+
+public class FullScreenTest extends SingleTouchTest {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+    }
+
+}
